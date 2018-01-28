80613980acab073f6727b76672f630b4c2ed90a9
Veggie13 <cmderochie+github@gmail.com>
2018-01-28 03:55:16 -0700

    Preliminary setup for the love interest game board.

--- RequestForProposal/Assets/Scripts/PlayerManager.cs
+++ RequestForProposal/Assets/Scripts/PlayerManager.cs
@@ -1,19 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
 
     public int PlayerCount;
+    public int LoveInterestPlayerID;
 
     [SerializeField]
     private GameObject SuitorBoardTemplate;
-    //private List<>
+    [SerializeField]
+    private GameObject LoveInterestBoard;
 
 	// Use this for initialization
 	void Start () {
 		for (int i = 1; i <= PlayerCount; i++)
         {
+            if (i == LoveInterestPlayerID)
+            {
+                continue;
+            }
             var suitorGO = Instantiate(SuitorBoardTemplate, new Vector3(), Quaternion.identity);
             suitorGO.transform.position = new Vector3(100 * i, 0, 0);
             suitorGO.GetComponentInChildren<WordManagerProxy>().Connect();
