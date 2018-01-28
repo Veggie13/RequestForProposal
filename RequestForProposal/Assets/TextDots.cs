1cf51942a1b74469c87c161a3f3522a1eca95bc2
Peter Harrison <thepeterharrison@gmail.com>
2018-01-27 17:39:31 -0700

    no message

--- RequestForProposal/Assets/TextDots.cs
+++ RequestForProposal/Assets/TextDots.cs
@@ -22,24 +22,32 @@ public class TextDots : MonoBehaviour {
             x++;
             Debug.Log("x = 0");
         }
+        else
+            return;
         if (x == 1)
         {
             waitingText.text = "Waiting for Lovers..";
             x++;
             Debug.Log("x = 1");
         }
+        else
+            return;
         if (x == 2)
-        { 
+        {
             waitingText.text = "Waiting for Lovers...";
             x++;
             Debug.Log("x = 2");
         }
+        else
+            return;
         if (x == 3)
         {
             waitingText.text = "Waiting for Lovers...";
             x = 0;
             Debug.Log("x = 3");
         }
+        else
+            return;
     }
 
 }
